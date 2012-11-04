51b2cf309cb84dbb6535b78c98919c4a3588a035
raqrodcha@gmail.com <raqrodcha@gmail.com@b0927d0a-d94a-ef7b-1e49-de7879ac50dc>
2012-11-04 15:35:29 +0000

    Layout Principal

---
+++ trunk/src/com/moneyorganizer/PantallaPrincipal.java
@@ -0,0 +1,20 @@
+package com.moneyorganizer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PantallaPrincipal extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_principal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_pantalla_principal, menu);
+        return true;
+    }
+}
