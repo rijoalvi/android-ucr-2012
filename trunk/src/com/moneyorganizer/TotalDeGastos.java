c69ad9698b09568fa92cff4c2d8002a27dd575d1
rijoalvi@gmail.com <rijoalvi@gmail.com@b0927d0a-d94a-ef7b-1e49-de7879ac50dc>
2012-11-12 07:29:11 +0000

    Trabajando en la carga de los datos de la BD al listview. Aun no esta listo :S

--- trunk/src/com/moneyorganizer/TotalDeGastos.java
+++ trunk/src/com/moneyorganizer/TotalDeGastos.java
@@ -1,10 +1,30 @@
 package com.moneyorganizer;
 
+import java.util.List;
+
+import com.moneyorganizer.db.ControladorBD;
+import com.moneyorganizer.elementos.Gasto;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class TotalDeGastos extends Activity {
 
@@ -27,5 +47,128 @@ public class TotalDeGastos extends Activity {
 
 	public void agregarGasto(View view) {
 		startActivity(new Intent(getApplicationContext(), CategoriaGasto.class));
+	}
+
+	private class GastoAdapter extends BaseAdapter {
+		List<Gasto> gastos;
+		LayoutInflater inflater;
+
+		public GastoAdapter(Context context, List<Gasto> gastos) {
+			this.gastos = gastos;
+			inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		public void addList(List<Gasto> gastos) {
+			this.gastos.addAll(gastos);
+			notifyDataSetChanged();// refresh
+		}
+
+		public void clear() {
+			this.gastos.clear();
+			notifyDataSetChanged();// refresh
+		}
+
+		public int getCount() {
+			return gastos.size();
+		}
+
+		public List<Gasto> getSongs() {
+			return gastos;
+		}
+
+		public Object getItem(int position) {
+			return gastos.get(position);
+		}
+
+		public void refresh() {
+			this.gastos = this.gastos;
+			notifyDataSetChanged();// refresh
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Gasto gastos = (Gasto) getItem(position);
+
+			GastoViewHolder holder = null;
+			if (convertView == null) {
+
+				convertView = inflater.inflate(R.layout.list_view_layout, parent,
+						false);
+
+				holder = new GastoViewHolder();
+
+				convertView.setTag(holder);
+
+				holder.lugar = (TextView) convertView
+						.findViewById(R.id.lugar_fuente);
+				holder.monto = (TextView) convertView
+						.findViewById(R.id.monto);
+				holder.fecha = (TextView) convertView
+						.findViewById(R.id.fecha);
+
+			} else {
+				holder = (GastoViewHolder) convertView.getTag();
+			}
+
+			holder.lugar.setText(gastos.getLugar());
+			holder.lugar.setTag(gastos.getId());
+			holder.monto.setText(gastos.getMonto());
+			holder.monto.setTag(gastos.getId());
+			String fechaGasto = String.valueOf(gastos.getDia())+"/"+String.valueOf(gastos.getMes())+"/"+String.valueOf(gastos.getAnio());
+			holder.fecha.setText(fechaGasto);
+			holder.fecha.setTag(gastos.getId());
+//			holder.favorite.setOnCheckedChangeListener(null);
+//			holder.favorite.setChecked(true);
+//			holder.favorite
+//					.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+//						public void onCheckedChanged(CompoundButton buttonView,
+//								boolean isChecked) {
+//							String id = buttonView.getTag().toString();
+//							if (id != null) {
+//
+//								final int songID = Integer.parseInt(id);
+//								AlertDialog.Builder dialog = new AlertDialog.Builder(
+//										Favorites.this);
+//								dialog.setTitle("Confirmation");
+//								dialog.setMessage("Do you really want to remove this favorite?");
+//								dialog.setPositiveButton("Si",
+//										new DialogInterface.OnClickListener() {
+//											public void onClick(
+//													DialogInterface dialog,
+//													int id) {
+//												Log.d("", "el ID es: " + id);
+//												removeSong(songID);
+//											}
+//										});
+//								dialog.setNegativeButton("No",
+//										new DialogInterface.OnClickListener() {
+//											public void onClick(
+//													DialogInterface dialog,
+//													int id) {
+//												MusicAdapter adapter = (MusicAdapter) ((HeaderViewListAdapter) mSongsList
+//														.getAdapter())
+//														.getWrappedAdapter();
+//												adapter.refresh();
+//
+//											}
+//										});
+//								dialog.show();
+//							}
+//
+//						}
+//					});
+			return convertView;
+		}
+	}
+
+	private static class GastoViewHolder {
+		public TextView fecha;
+		public TextView monto;
+		public TextView lugar;
+		public ImageView imagenGasto;
 	}
 }
